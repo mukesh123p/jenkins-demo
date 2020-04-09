67c6dc93c52a6430ddc8fec8ba2ccfcdf2c14515
mukesh123p <43925461+mukesh123p@users.noreply.github.com>
2020-04-10 00:34:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('this is a website created by me as mukesh');
 });
 
 var server = app.listen(3000, function () {
